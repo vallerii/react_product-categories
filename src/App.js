c44d95e4c79e69aa576191276760155cc7917323
mgrinko <glukym@gmail.com>
2020-04-24 08:16:15 +0300

    Basic markup was restored

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React from 'react';
+
 import './App.scss';
-import Note from './components/Note';
-import { tasks } from './api/tasks';
-import { notes } from './api/notes';
 
-const preparedNotes = notes.map((note) => {
-  const getTaskById = taskId => tasks.find(task => task.id === taskId);
-
-  return {
-    ...note,
-    tasks: note.tasks.map(getTaskById),
-  };
-});
+// import { tasks } from './api/tasks';
+// import { notes } from './api/notes';
 
 function App() {
   return (
     <div className="container">
-      {preparedNotes.map(note => (
-        <Note {...note} key={note.id} />
-      ))}
+      <div className="ui card">
+        <div className="ui content">
+          <div className="ui description">
+            <p>Sunday</p>
+            <ul className="ui list">
+              <li>Banana</li>
+              <li>Beer</li>
+              <li>Cake</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="ui card">
+        <div className="ui content">
+          <div className="ui description">
+            <p>My day</p>
+            <b>No tasks</b>
+          </div>
+        </div>
+      </div>
+
+      <div className="ui card">
+        <div className="ui content">
+          <div className="ui description">
+            <p>Work</p>
+            <ul className="ui list">
+              <li>Sugar</li>
+              <li>Coffee</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="ui card">
+        <div className="ui content">
+          <div className="ui description">
+            <p>Monday</p>
+            <ul className="ui list">
+              <li>Milk</li>
+              <li>Bread</li>
+              <li>Eggs</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="ui card">
+        <div className="ui content">
+          <div className="ui description">
+            <p>Everyday</p>
+            <ul className="ui list">
+              <li>Cheese</li>
+              <li>Sugar</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
